a9e3794bce453ac35f4574101cf380867614ebf7
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-08-30 16:47:35 +0200

    create header add header css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Home.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>This is HOMEPAGE</h1>
+
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link t0='/images'>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
+                </div>
+            </div>
+
+            <div className="home__body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
